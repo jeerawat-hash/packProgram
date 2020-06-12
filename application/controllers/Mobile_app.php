aa925d3eb056214e2c034e03601f0cce1eaaf6a1
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-06-12 21:23:01 +0700

    add

--- application/controllers/Mobile_app.php
+++ application/controllers/Mobile_app.php
@@ -21,67 +21,24 @@ class Mobile_app extends CI_Controller
 			$header['page_focus'] = 'home';
 			 
 
-
 			$this->load->view("page/template_mobile/header",$header);
 			$this->load->view("page/mobile/mobile_home");
 			$this->load->view("page/template_mobile/footer");
 			
-		  
 	}
-	public function login()
-	{
-
-		$Telephone = "";
- 		if (isset($_POST["Telephone"])) {
- 			$Telephone = $_POST["Telephone"];
- 		}
-
-		echo  json_encode( $this->Mobile_model->CheckCustomerByTelephone($Telephone) );
-
-
-	}
-	public function logout()
-	{
- 
-			redirect("Mobile_app");
-			 
-
-	}
-
-	public function getDataCustomerCableRent()
+	public function createDataFromXlsx()
 	{
 
 
-		echo json_encode($this->Mobile_model->getDataCustomerCableRent($_POST["Telephone"]));
+		$result = $this->Mobile_model->createDataFromXlsx();
+
+
+
+
 
 
 	}
-	public function getDataCustomerServicesAllCable()
-	{ 
-		
-		echo json_encode($this->Mobile_model->getDataCustomerServicesAllCable($_POST["Telephone"]));
 
-
-	}
-	public function updateResyncCard()
-	{
-
- 
-		foreach ($_POST["CardNO"] as $Card) {
-			$this->Mobile_model->reSyncServicesCable($_POST["Telephone"],$Card);
-		}
-
-		echo 1;
-
-
-	}
-	public function getDataPPPOEStatusByTelephone()
-	{
-
-		echo json_encode($this->Mobile_model->getDataPPPOEStatusByTelephone($_POST["Telephone"]));
-		
-
-	}
 
 
 
