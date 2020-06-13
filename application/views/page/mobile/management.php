1f33ea5b5d06539f4bd811a4d8440b434beca3ea
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-06-13 18:15:15 +0700

    add

--- application/views/page/mobile/management.php
+++ application/views/page/mobile/management.php
@@ -398,7 +398,7 @@
        
 <script type="text/javascript">
         
-        var player = new Clappr.Player({source: "http://gcorigin.wisdomcloud.net/live/master/playlist.m3u8_1000.m3u8", parentId: "#player"});
+        var player = new Clappr.Player({source: "https://gcorigin.wisdomcloud.net/live/master/playlist.m3u8_1000.m3u8", parentId: "#player"});
     
 
 </script>
