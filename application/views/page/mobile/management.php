70d0f9ff60984797ed642323a3eb921a23cfe26c
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-06-13 16:08:26 +0700

    add

--- application/views/page/mobile/management.php
+++ application/views/page/mobile/management.php
@@ -63,6 +63,18 @@
                     </div>
 
                 </div> 
+
+                <div id="SendSyncCarInfo" class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <div class="info-box hover-zoom-effect">
+                        <div class="icon bg-red">
+                            <i class="material-icons">email</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">แก้ไขข้อมูลทะเบียนรถ</div>
+                        </div>
+                    </div>
+
+                </div> 
   
  
             <!-- Menu -->
