bd0a56a75105c79c808c525d9eddc1dba3ce2d83
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-06-13 18:50:27 +0700

    add

--- assets/pages/manage/js/Manage_CCTV.js
+++ assets/pages/manage/js/Manage_CCTV.js
@@ -16,8 +16,8 @@ $(function(){
     var player = new Clappr.Player({
         source: "https://gcorigin.wisdomcloud.net/live/master/playlist.m3u8_1000.m3u8",
         preload: "none",
-        width: '20%',
-        height: '20%',
+        width: '50%',
+        height: '50%',
         parentId: "#CCTVplayer"
     });
 
