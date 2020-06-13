664c8cfd47f1cb7804f10acf17e3c2746ef4be39
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-06-13 18:52:38 +0700

    add

--- assets/pages/manage/js/Manage_CCTV.js
+++ assets/pages/manage/js/Manage_CCTV.js
@@ -22,6 +22,25 @@ $(function(){
     });
 
 
+        ////////////
+
+    });
+
+    $("#CCTV2").on("click",function(){
+
+        $("#CCTVplayer").html("");
+
+        $("#CCTVModal").modal("show");
+        ////////////
+     
+    var player = new Clappr.Player({
+        source: "https://edge2-bkk.3bb.co.th:9443/CartoonClub_Livestream/cartoonclub_480P.stream/chunklist_w66866808.m3u8",
+        preload: "none",
+        width: '50%',
+        height: '50%',
+        parentId: "#CCTVplayer"
+    });
+
 
         ////////////
 
