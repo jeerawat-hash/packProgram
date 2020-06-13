f4b2380499a8036e4b21bcd4ca4e82d84e13b16d
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-06-13 18:35:04 +0700

    add

--- assets/pages/manage/js/Manage_CCTV.js
+++ assets/pages/manage/js/Manage_CCTV.js
@@ -6,9 +6,16 @@ $(function(){
 
         $("#CCTVModal").modal("show");
         ////////////
+     
+    var player = new Clappr.Player({
+        source: "https://gcorigin.wisdomcloud.net/live/master/playlist.m3u8_1000.m3u8",
+        preload: "none",
+        width: "100%",
+        hideMediaControl: false,
+        parentId: "#CCTVplayer"
+    });
 
-        var player = new Clappr.Player({source: "https://gcorigin.wisdomcloud.net/live/master/playlist.m3u8_1000.m3u8", parentId: "#CCTVplayer"});
- 
+
 
         ////////////
 
