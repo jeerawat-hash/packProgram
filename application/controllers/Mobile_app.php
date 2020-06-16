788c163a3a18aa1d9d7d1c7b6441eac37539d357
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-06-16 15:59:11 +0700

    dd

--- application/controllers/Mobile_app.php
+++ application/controllers/Mobile_app.php
@@ -57,20 +57,9 @@ class Mobile_app extends CI_Controller
 		//print_r($_POST);
 		//print_r($_FILES);
  		
- 		
-
+ 
 			move_uploaded_file($_FILES["Slipfile"]["tmp_name"], "/home/admin/web/pack1.sakorncable.com/public_html/upload/temp/".$_POST["CustomerID"].".jpg");
-
-
-			if ( !isset($_FILES["Slipfile"]["type"]) ) {
- 				echo "2";
- 				exit();
-	 		}
-	 		if ($_FILES["Slipfile"]["type"] != "image/png") {
-	 			echo "2";
-	 			exit();
-	 		}
-
+ 
 
 			$Member = $this->Mobile_model->getDataBlanace($_POST["CustomerID"]);				
 
@@ -82,9 +71,13 @@ class Mobile_app extends CI_Controller
 	        		
 	        	shell_exec(" rm  /home/admin/web/pack1.sakorncable.com/public_html/upload/temp/".$_POST["CustomerID"].".jpg");
 
+	        	echo "1";
+
+	        }else{
+	        	echo "2";
 	        }
 
-	    echo "1";
+	    
   
 
 
