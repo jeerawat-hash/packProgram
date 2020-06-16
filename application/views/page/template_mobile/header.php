2a234863bb463e68a60bc33f0dac1d8966c42c93
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-06-16 22:53:58 +0700

    add

--- application/views/page/template_mobile/header.php
+++ application/views/page/template_mobile/header.php
@@ -741,10 +741,10 @@
             <!-- Footer -->
             <div class="legal">
                 <div class="copyright">
-                    &copy; 2016 - 2017 <a href="javascript:void(0);">AdminBSB - Material Design</a>.
+                    &copy; 2019-2020 <a href="javascript:void(0);">SakornCable NetworkServices</a>.
                 </div>
                 <div class="version">
-                    <b>Version: </b> 1.0.5
+                    <b>Version: </b> 0.10p
                 </div>
             </div>
             <!-- #Footer -->
