cb5915e2ab9aff7ac5dbdc0f7fd65b730f2ffd15
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-06-16 13:37:02 +0700

    ad

--- assets/pages/mobile/js/Mobile_CustomerSendSlip.js
+++ assets/pages/mobile/js/Mobile_CustomerSendSlip.js
@@ -3,7 +3,26 @@ $(function(){
     
 	$("#CustomerSendSlip").on("click",function(){
 
+
+		$("#CustomerSendSlipModal").find("#PreloadSendData").hide();
+		$("#CustomerSendSlipModal").find("#SendData").show();
+		$("#CustomerSendSlipModal").find("#Slipfile").val("");
 		$("#CustomerSendSlipModal").modal("show");
+  
+
+	});
+
+	$("#CustomerSendSlipModal").find("#SendData").on("click",function(){
+
+		$("#CustomerSendSlipModal").find("#SendData").hide();
+		$("#CustomerSendSlipModal").find("#PreloadSendData").show();
+
+
+
+
+
+
+
 
 	});
 
