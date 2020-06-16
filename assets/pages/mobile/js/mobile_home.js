91752ff5eef7a3b122e870a2c50c2c0ea33661b4
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-06-16 14:48:18 +0700

    addd

--- assets/pages/mobile/js/mobile_home.js
+++ assets/pages/mobile/js/mobile_home.js
@@ -39,10 +39,10 @@ $(function(){
 			,function(data,status){
 
 
-				//var object =  JSON.parse(data);
+				var object =  JSON.parse(data);
 
-				console.log(data);
-				//console.log(object);
+				//console.log(data);
+				console.log(object);
 
 				/*
 				if (data == 1) {
