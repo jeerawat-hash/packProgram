150ca2cbc6cbfd337f0dd1d819d8fb1a60f523df
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-06-16 22:33:16 +0700

    add

--- application/views/page/mobile/mobile_home.php
+++ application/views/page/mobile/mobile_home.php
@@ -290,7 +290,7 @@
                                         <label>ชื่อบัญชี : นิติบุคคลอาคารชุดเคหะชุมชนและบริการชุมชนเทพารักษ์ 3/1</label>
                                         <br>
                                         <button id="CopyBankCode" onclick="CopyFunction()" class="btn btn-lg btn-info waves-effect" >กดเพื่อคัดลอกหมายเลขบัญชี</button>
-                                        <input type="text" value="3994048883" name="BankCode" id="BankCode">
+                                        <input type="text" style="display: none;" value="3994048883" name="BankCode" id="BankCode">
                                     </div>
                                 </div>
 
