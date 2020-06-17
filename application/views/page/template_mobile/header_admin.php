93e0e4feedb7966aff976e16edb111ebd9c94798
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-06-17 12:46:04 +0700

    add

--- application/views/page/template_mobile/header_admin.php
+++ application/views/page/template_mobile/header_admin.php
@@ -19,38 +19,13 @@
 
 <script type="text/javascript">
      
-
-     // ==UserScript==
-// @name         Anti Anti-debugger
-// @namespace    http://tampermonkey.net/
-// @version      1
-// @description  Stops most anti debugging implementations by JavaScript obfuscaters
-// @author       ww
- @match        *
- @grant        unsafeWindow
-// @run-at       document-start
-// ==/UserScript==
-
-(function() {
-    var _constructor = unsafeWindow.Function.prototype.constructor;
-    // Hook Function.prototype.constructor
-    unsafeWindow.Function.prototype.constructor = function() {
-        var fnContent = arguments[0];
-        if (fnContent) {
-            if (fnContent.includes('debugger')) { // An anti-debugger is attempting to stop debugging
-                var caller = Function.prototype.constructor.caller; // Non-standard hack to get the function caller
-                var callerContent = caller.toString();
-                if (callerContent.includes(/\bdebugger\b/gi)) { // Eliminate all debugger statements from the caller, if any
-                    callerContent = callerContent.replace(/\bdebugger\b/gi, ''); // Remove all debugger expressions
-                    eval('caller = ' + callerContent); // Replace the function
-                }
-                return (function () {});
-            }
-        }
-        // Execute the normal function constructor if nothing unusual is going on
-        return _constructor.apply(this, arguments);
-    };
-})();
+unsafeWindow.SomeVarInPage = "Testing";
+unsafeWindow.SomeFunctionInPage("Test");
+var oldFunction = unsafeWindow.SomeFunctionInPage;
+unsafeWindow.SomeFunctionInPage = function(text) {
+  alert('Hijacked! Argument was ' + text + '.');
+  return oldFunction(text);
+};
 
 
 
