f73e6f7cdd3f0ca04b018ddd9a16fb6c3d03e54a
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-06-17 12:51:31 +0700

    add

--- application/views/page/template_mobile/header_admin.php
+++ application/views/page/template_mobile/header_admin.php
@@ -18,24 +18,9 @@
 ?>
 
 <script type="text/javascript">
-// Just a normal eval
-eval("console.log('1337')");
-// Now we repat the process...
-var original = eval;
-var fake = function(argument) {
-    // If the code to be evaluated contains 1337...
-    if (argument.indexOf("1337") !== -1) {
-        // ... we just execute a different code
-        original("for (i = 0; i < 10; i++) { console.log(i);}");
-    }
-    else {
-        original(argument);
-    }
-}
-eval = fake;
-eval("console.log('We should see this...')");
-// Now we should see the execution of a for loop instead of what is expected
-eval("console.log('Too 1337 for you!')");
+console.log("See me!");
+debugger;
+console.log("See me!");
 
 
 
