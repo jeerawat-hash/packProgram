70030b25608a88dbe2f1b3675f46366811f94e74
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-06-17 16:38:22 +0700

    add

--- application/views/page/mobile/mobile_home.php
+++ application/views/page/mobile/mobile_home.php
@@ -512,8 +512,6 @@
                                     
                                     <!-- embed responsive iframe --> 
                                     <!-- ======================= -->
-
-                                    <div id="Iframe-Master-CC-and-Rs" class="set-margin set-padding set-border set-box-shadow center-block-horiz">
                                       <div class="responsive-wrapper 
                                          responsive-wrapper-wxh-572x612"
                                          style="-webkit-overflow-scrolling: touch; overflow: auto;">
@@ -523,8 +521,7 @@
                                     An &#105;frame should be displayed here but your browser version does not support &#105;frames. </em>Please update your browser to its most recent version and try again.</p>
                                         </iframe>
                                         
-                                      </div>
-                                    </div>
+                                      </div> 
  
 
 
