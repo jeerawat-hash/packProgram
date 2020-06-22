0170eeb1c921ba014709bf5da7c1e8cdd2b6a3bc
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-06-22 16:05:20 +0700

    add

--- application/views/page/mobile/mobile_home.php
+++ application/views/page/mobile/mobile_home.php
@@ -833,8 +833,6 @@
 
 
 <script>
-//alert(atob('aHR0cHM6Ly9hcHAuc2Frb3JuY2FibGUuY29tL2hscy9jY3R2MjQtLm0zdTg='));
-//alert(btoa('use javascript btoa to encode data in base64 format'));
 
 
 function CopyFunction() {
