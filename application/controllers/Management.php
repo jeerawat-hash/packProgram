66999056f1b97652ff2ccbfc478e3a00919b6a96
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-06-23 12:52:16 +0700

    add

--- application/controllers/Management.php
+++ application/controllers/Management.php
@@ -137,9 +137,9 @@ class Management extends CI_Controller
 	{
 
 
-		//print_r($_FILES);
+		print_r($_FILES);
 
-
+/*
 		if ($_FILES["CustomerName"]["type"] == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") {
 
 			shell_exec("rm /home/admin/web/pack1.sakorncable.com/public_html/upload/temp/".$_FILES["CustomerName"]["name"]);
@@ -149,14 +149,14 @@ class Management extends CI_Controller
 
 			$result = $this->Mobile_model->createDataFromXlsx($_FILES["CustomerName"]["name"]);
 
-			/*foreach ($result as $Value) {
+			foreach ($result as $Value) {
 
 				//print_r($Value);
 				echo $Value["PE_CODE"]." ".$Value["PE_NAME"];
 				//$this->Mobile_model->SyncDataCustomerName($Value["PE_CODE"],$Value["PE_TITLE"],$Value["PE_NAME"]);
 
 
-			}*/
+			}
 
 			print_r($result);
 
@@ -168,7 +168,7 @@ class Management extends CI_Controller
 			echo "2";
 
 		}
- 
+ */
  
 
 	}
