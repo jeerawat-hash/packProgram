0c58a5473d4b5374e0a7090825fcc809d4a7ca8d
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-06-23 12:59:17 +0700

    add

--- application/controllers/Management.php
+++ application/controllers/Management.php
@@ -152,7 +152,11 @@ class Management extends CI_Controller
 			foreach ($result as $Value) {
 
 				//print_r($Value);
-				echo $Value["PE_CODE"]." ".$Value["PE_NAME"];
+				if ($Value["PE_CODE"] == "042-38") {
+					
+					echo $Value["PE_CODE"]." ".$Value["PE_NAME"];
+					
+				}
 				//$this->Mobile_model->SyncDataCustomerName($Value["PE_CODE"],$Value["PE_TITLE"],$Value["PE_NAME"]);
 
 
