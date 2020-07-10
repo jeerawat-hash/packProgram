70a09c3f89620f421035d0d8dd3de72f17c6f0b1
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-07-11 00:17:30 +0700

    add

--- application/models/Devices_model.php
+++ application/models/Devices_model.php
@@ -3,6 +3,26 @@
 class Devices_model extends CI_Model
 {	
   
+    public function insertDataTemp($ImgUrl,$temp,$DateStamp,$TimeStamp)
+    {
+
+       $this->mssql = $this->load->database("mssql",true);
+
+
+       $this->mssql->query("INSERT INTO [WebSakorn].[dbo].[TempProject]
+           ([URLIMG]
+           ,[Temp] 
+           ,[DateStamp]
+           ,[TimeStamp])
+     VALUES
+           ('".$ImgUrl."'
+           ,'".$temp."'
+           ,'".$DateStamp."'
+           ,'".$TimeStamp."')");
+     
+ 
+ 
+    }
 
 
 
