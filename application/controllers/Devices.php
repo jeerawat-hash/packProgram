755fb10f0bdfa9974d7e0611348db60479c97c3e
jeerawat-hash <54786014+jeerawat-hash@users.noreply.github.com>
2020-07-13 11:31:53 +0700

    Update Devices.php

--- application/controllers/Devices.php
+++ application/controllers/Devices.php
@@ -23,16 +23,16 @@ class Devices extends CI_Controller
 	public function tempupload()
 	{
 
-		//print_r($_FILES["/home/pi/Desktop/output_jpg"]);
-		//print_r($_POST);
+		print_r($_FILES);
+		print_r($_POST);
 
-		$file = $_FILES["/home/pi/Desktop/output_jpg"]["name"]."-".$_POST["Date"]."-".$_POST["Time"];
+		//$file = $_FILES["/home/pi/Desktop/output_jpg"]["name"]."-".$_POST["Date"]."-".$_POST["Time"];
 
-		move_uploaded_file($_FILES["/home/pi/Desktop/output_jpg"]["tmp_name"], "/home/admin/web/pack1.sakorncable.com/public_html/upload/tempimg/".$file.".jpg");
+		//move_uploaded_file($_FILES["/home/pi/Desktop/output_jpg"]["tmp_name"], "/home/admin/web/pack1.sakorncable.com/public_html/upload/tempimg/".$file.".jpg");
 
-		$ImgUrl = "http://pack1.sakorncable.com/upload/tempimg/".$file.".jpg";
+		//$ImgUrl = "http://pack1.sakorncable.com/upload/tempimg/".$file.".jpg";
 
-		 $this->Devices_model->insertDataTemp($ImgUrl,$_POST["Temp"],$_POST["Date"],$_POST["Time"]);
+		//$this->Devices_model->insertDataTemp($ImgUrl,$_POST["Temp"],$_POST["Date"],$_POST["Time"]);
 
 
 	}
