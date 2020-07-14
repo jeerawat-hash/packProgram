e7a9721090bc263cf6dd777525b20fe020a42db0
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-07-14 11:08:41 +0700

    add

--- application/views/direct/temperature.php
+++ application/views/direct/temperature.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
-    <link href="cover.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/4.0/examples/cover/cover.css" rel="stylesheet">
   </head>
 
   <body class="text-center">
