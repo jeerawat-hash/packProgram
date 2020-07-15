48418330edbe7243708272e6b8f6bb0e42f8cafc
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-07-15 21:31:38 +0700

    test

--- application/views/direct/test.php
+++ application/views/direct/test.php
@@ -47,78 +47,11 @@
         <!-- ============================================================== -->
         <header class="topbar">
             <nav class="navbar top-navbar navbar-expand-md navbar-dark">
-                <div class="navbar-header">
-                    <!-- This is for the sidebar toggle which is visible on mobile only -->
-                    <a class="nav-toggler waves-effect waves-light d-block d-md-none" href="javascript:void(0)"><i
-                            class="ti-menu ti-close"></i></a>
-                    <!-- ============================================================== -->
-                    <!-- Logo -->
-                    <!-- ============================================================== -->
-                    <a class="navbar-brand" href="index.html">
-                        <!-- Logo icon -->
-                        <b class="logo-icon">
-                            <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
-                            <!-- Dark Logo icon -->
-                            <img src="https://pack1.sakorncable.com/assets_new/images/logo-iconnn.png" alt="homepage" class="dark-logo"  width="50px" height="60px" />
-                            <!-- Light Logo icon -->
-                            <img src="https://pack1.sakorncable.com/assets_new/images/logo-light-icon.png" alt="homepage" class="light-logo" />
-                        </b>
-                        <!--End Logo icon -->
-                        <!-- Logo text -->
-                        <span class="logo-text">
-                            <!-- dark Logo text -->
-                           <!-- <img src="../assets/images/logo-text.png" alt="homepage" class="dark-logo" />-->
-                            <!-- Light Logo text -->
-                          <!--  <img src="../assets/images/logo-light-text.png" class="light-logo" alt="homepage" />-->
-                        </span>
-                    </a>
-                    <!-- ============================================================== -->
-                    <!-- End Logo -->
-                    <!-- ============================================================== -->
-                    <!-- ============================================================== -->
-                    <!-- Toggle which is visible on mobile only -->
-                    <!-- ============================================================== -->
-                    <a class="topbartoggler d-block d-md-none waves-effect waves-light" href="javascript:void(0)"
-                        data-toggle="collapse" data-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i
-                            class="ti-more"></i></a>
-                </div>
+                
                 <!-- ============================================================== -->
                 <!-- End Logo -->
                 <!-- ============================================================== -->
-                <div class="navbar-collapse collapse" id="navbarSupportedContent">
-                    <!-- ============================================================== -->
-                    <!-- toggle and nav items -->
-                    <!-- ============================================================== -->
-                    <ul class="navbar-nav mr-auto">
-                        <!-- This is  -->
-                        <li class="nav-item"> <a
-                                class="nav-link sidebartoggler d-none d-md-block waves-effect waves-dark"
-                                href="javascript:void(0)"><i class="ti-menu"></i></a> </li>
-                        <!-- ============================================================== -->
-                        <!-- Search -->
-                        <!-- ============================================================== -->
-                        <li class="nav-item d-none d-md-block search-box"> <a
-                                class="nav-link d-none d-md-block waves-effect waves-dark" href="javascript:void(0)"><i
-                                    class="ti-search"></i></a>
-                            <form class="app-search">
-                                <input type="text" class="form-control" placeholder="Search & enter"> 
-                                <a class="srh-btn"><i class="ti-close"></i></a> 
-                            </form>
-                        </li>
-                        <!-- ============================================================== -->
-                        <!-- Mega Menu -->
-                        <!-- ============================================================== -->
-                        
-                        <!-- ============================================================== -->
-                        <!-- End Mega Menu -->
-                        <!-- ============================================================== -->
-                    </ul>
-                    <!-- ============================================================== -->
-                    <!-- Right side toggle and nav items -->
-                    <!-- ============================================================== -->
-                   
-                </div>
+                 
             </nav>
         </header>
         <!-- ============================================================== -->
