aa5cc8bb9466a7ac823fcf904ef8b298389e15c1
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-07-15 21:38:56 +0700

    tesr

--- application/views/direct/test.php
+++ application/views/direct/test.php
@@ -26,6 +26,8 @@
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 <![endif]-->
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
 </head>
 
 <body>
@@ -201,6 +203,52 @@
     <script src="https://pack1.sakorncable.com/assets_new/libs/c3/c3.min.js"></script>
     <!-- Chart JS -->
     <script src="https://pack1.sakorncable.com/dist/js/pages/dashboards/dashboard1.js"></script>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+
+
+    <script type="text/javascript">
+        
+
+      $(function(){
+
+        
+
+        setInterval(function(){ 
+
+
+          $.post("https://pack1.sakorncable.com/index.php/devices",function(data,status){
+
+            var object = JSON.parse(data);
+            //console.log(object[0]);
+            var APPURL = "https://pack1.sakorncable.com/index.php/devices/app/"+object[0].ID;
+
+            $("#ImageCapture").attr("src",object[0].URLIMG);
+            $("#QRCodeApp").attr("src","https://api.qrserver.com/v1/create-qr-code/?data="+APPURL+"&size=220x220&margin=0");
+            $("#Temp").text(object[0].Temp);
+
+
+          });
+          //console.log("update");
+
+        }, 1000);
+
+
+
+      });
+
+
+
+    </script>
+
+
 </body>
 
 
