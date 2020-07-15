3be4874dba203c4c5ea3c2dc09943e796f211120
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-07-15 21:54:52 +0700

    twsr

--- application/views/direct/test.php
+++ application/views/direct/test.php
@@ -44,16 +44,7 @@
     <div id="main-wrapper">
         <!-- ============================================================== -->
         <!-- Topbar header - style you can find in pages.scss -->
-        <!-- ============================================================== -->
-        <header class="topbar">
-            <nav class="navbar top-navbar navbar-expand-md navbar-dark">
-                
-                <!-- ============================================================== -->
-                <!-- End Logo -->
-                <!-- ============================================================== -->
-                 
-            </nav>
-        </header>
+       
         <!-- ============================================================== -->
         <!-- End Topbar header -->
         <!-- ============================================================== -->
@@ -73,18 +64,18 @@
             <!-- ============================================================== -->
             <div class="row page-titles">
                 <div class="col-md-5 col-12 align-self-center">
-                    <h3 class="text-themecolor mb-0">Dashboard</h3>
+                    <h3 class="text-themecolor mb-0">วัดอุณหภูมิ</h3>
                     <ol class="breadcrumb mb-0">
-                        <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-                        <li class="breadcrumb-item active">Dashboard</li>
+                        <li class="breadcrumb-item"><a href="javascript:void(0)">หน้าแรก</a></li>
+                        <li class="breadcrumb-item active">วัดอุณหภูมิ</li>
                     </ol>
                 </div>
                 <div class="col-md-7 col-12 align-self-center d-none d-md-block">
                     <div class="d-flex mt-2 justify-content-end">
                         <div class="d-flex mr-3 ml-2">
                             <div class="chart-text mr-2">
-                                <h6 class="mb-0"><small>THIS MONTH</small></h6>
-                                <h4 class="mt-0 text-info">$58,356</h4>
+                                <h6 class="mb-0"><small>`จำนวนคน</small></h6>
+                                <h4 class="mt-0 text-info">58,356</h4>
                             </div>
                             <div class="spark-chart">
                                 <div id="monthchart"></div>
@@ -92,8 +83,8 @@
                         </div>
                         <div class="d-flex ml-2">
                             <div class="chart-text mr-2">
-                                <h6 class="mb-0"><small>LAST MONTH</small></h6>
-                                <h4 class="mt-0 text-primary">$48,356</h4>
+                                <h6 class="mb-0"><small>ค่าเฉลี่ย</small></h6>
+                                <h4 class="mt-0 text-primary">$8,356</h4>
                             </div>
                             <div class="spark-chart">
                                 <div id="lastmonthchart"></div>
@@ -126,7 +117,7 @@
                                         </div>
                                     </div>
                                      <div class="col-12">
-                                        <div  style="height: 360px;"><center><img id="ImageCapture" width="90%" src="https://pack1.sakorncable.com/upload/tempimg/output.jpg-2020-07-13-06:51:27.jpg"></center></div>
+                                        <div  style="height: 360px;"><center><img id="ImageCapture" width="60%" src="https://pack1.sakorncable.com/upload/tempimg/output.jpg-2020-07-13-06:51:27.jpg"></center></div>
                                     </div>
                                      
                                 </div>
