91510459f9bc5a3250427ebbab52daf0515f56af
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-07-16 09:35:13 +0700

    add

--- application/views/direct/temperature.php
+++ application/views/direct/temperature.php
@@ -1,105 +1,186 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
 
-<!doctype html>
-<html lang="en">
-  <head>
+
+<!-- Mirrored from www.wrappixel.com/demos/admin-templates/materialpro-bootstrap-latest/material-pro/src/minisidebar/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 15 Jul 2020 01:41:16 GMT -->
+<head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="https://pack1.sakorncable.com/assets_new/images/favicon.png">
+    <title>KMUTNB Temperature measurement </title>
+    <link rel="canonical" href="https://www.wrappixel.com/templates/xtremeadmin/" />
+    <link href="https://pack1.sakorncable.com/assets_new/libs/chartist/dist/chartist.min.css" rel="stylesheet">
+    <link href="https://pack1.sakorncable.com/dist/js/pages/chartist/chartist-init.css" rel="stylesheet">
+    <link href="https://pack1.sakorncable.com/assets_new/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.css" rel="stylesheet">
+    <link href="https://pack1.sakorncable.com/assets_new/libs/c3/c3.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="https://pack1.sakorncable.com/dist/css/style.min.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
 
-    <title>รายงานผลการตรวจคัดกรอง</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/cover/">
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-    <!-- Custom styles for this template -->
-  </head>
-
-  <body class="text-center">
-
-    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
-      <header class="masthead mb-auto">
-        <center>
-        <h1 class="cover-heading">ผลการตรวจคัดกรองอุณหภูมิ.</h1>
-        </center>
-        <div class="inner">
-           <!-- <h3 class="masthead-brand">รายงานผลการตรวจคัดกรอง</h3> -->
-          <nav class="nav nav-masthead justify-content-center">
-           <!-- <a class="nav-link active" href="#">ตรวจสอบ</a>  -->
-          </nav>
+<body>
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="lds-pos"></div>
+            <div class="lds-pos"></div>
         </div>
-      </header> 
-
-      <div class="container">
-          
-          <div class="row">
-              <div class="col-lg-6">
-                
-                <label>จัดเก็บข้อมูลภาพ</label>
-                <img id="ImageCapture" width="100%" src="https://pack1.sakorncable.com/upload/tempimg/output.jpg-2020-07-13-06:51:27.jpg">
-
-              </div>
-
-              <br>
- 
-              <div class="col-lg-6">
-                
-                <label>สแกนเพื่อบันทึกข้อมูลและเช็คอินไทยชนะ</label>
-                <img id="QRCodeApp" src="https://api.qrserver.com/v1/create-qr-code/?data=https://pack1.sakorncable.com/upload/tempimg/output.jpg-2020-07-13-06:51:27.jpg&size=220x220&margin=0">
-                <br>
-                <br>
-                    <div class="row">
-                      <div class="col-12">
-                        <h3 >
+    </div>
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper">
+        <!-- ============================================================== -->
+        <!-- Topbar header - style you can find in pages.scss -->
+       
+        <!-- ============================================================== -->
+        <!-- End Topbar header -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+       
+        <!-- ============================================================== -->
+        <!-- End Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page wrapper  -->
+        <!-- ============================================================== -->
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <div class="row page-titles">
+                <div class="col-md-5 col-12 align-self-center">
+                    <h3 class="text-themecolor mb-0">วัดอุณหภูมิ</h3>
+                     
+                </div>
+                <div class="col-md-7 col-12 align-self-center d-none d-md-block">
+                    <div class="d-flex mt-2 justify-content-end">
+                        <div class="d-flex mr-3 ml-2">
+                            <div class="chart-text mr-2">
+                                <h6 class="mb-0"><small>จำนวนคน</small></h6>
+                                <h4 class="mt-0 text-info"><label id="CountTotal"> Nan </label> คน</h4>
+                            </div>
+                            <div class="spark-chart">
+                                <div id="monthchart"></div>
+                            </div>
+                        </div>
+                        <div class="d-flex ml-2">
+                            <div class="chart-text mr-2">
+                                <h6 class="mb-0"><small>ค่าเฉลี่ย</small></h6>
+                                <h4 class="mt-0 text-primary"><label id="TempAVG">37</label>°C</h4>
+                            </div>
+                            <div class="spark-chart">
+                                <div id="lastmonthchart"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <!-- Row -->
+                <div class="row">
+                    <!-- Column -->
+                    <div class="col-lg-8 col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="d-flex flex-wrap">
+                                            <div>
+                                                <h3 class="card-title">ผลการตรวจคัดกรองอุณหภูมิ</h3>
+                                                <h6 class="card-subtitle">จัดเก็บข้อมูลภาพ</h6>
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                     <div class="col-12">
+                                        <div  style="height: 360px;"><center><img id="ImageCapture" width="60%" src="https://pack1.sakorncable.com/upload/tempimg/output.jpg-2020-07-13-06:51:27.jpg"></center></div>
+                                    </div>
+                                     
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h3 class="card-title">สแกนเพื่อบันทึกข้อมูลและเช็คอินไทยชนะ </h3>
+                                <h6 class="card-subtitle"></h6>
+                                <div   style="height:360px; width:100%;"><center><br> <img id="QRCodeApp" src="https://api.qrserver.com/v1/create-qr-code/?data=https://pack1.sakorncable.com/upload/tempimg/output.jpg-2020-07-13-06:51:27.jpg&size=220x220&margin=0"><br>  
+                                <div class="col-12">
+                        <h3 ><br><br>
                         <label>อุณหภูมิ :</label><label id="Temp">36.7 </label><label>°C</label>
                         </h3>
                       </div>
+                           </center></div>
+                            </div>
+                            
+                        </div>
                     </div>
- 
-
-              </div>
-          </div>
-
+                </div>
+                <!-- Row -->
+                   
+            </div>
         </div>
+    </aside>
+    <div class="chat-windows"></div>
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="https://pack1.sakorncable.com/assets_new/libs/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="https://pack1.sakorncable.com/assets_new/libs/popper.js/dist/umd/popper.min.js"></script>
+    <script src="https://pack1.sakorncable.com/assets_new/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- apps -->
+    <script src="https://pack1.sakorncable.com/dist/js/app.min.js"></script>
+    <script src="https://pack1.sakorncable.com/dist/js/app.init.mini-sidebar.js"></script>
+    <script src="https://pack1.sakorncable.com/dist/js/app-style-switcher.js"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="https://pack1.sakorncable.com/assets_new/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+    <script src="https://pack1.sakorncable.com/assets_new/extra-libs/sparkline/sparkline.js"></script>
+    <!--Wave Effects -->
+    <script src="https://pack1.sakorncable.com/dist/js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="https://pack1.sakorncable.com/dist/js/sidebarmenu.js"></script>
+    <!--Custom JavaScript -->
+    <script src="https://pack1.sakorncable.com/dist/js/custom.min.js"></script>
+    <!--This page JavaScript -->
+    <!-- chartist chart -->
+    <script src="https://pack1.sakorncable.com/assets_new/libs/chartist/dist/chartist.min.js"></script>
+    <script src="https://pack1.sakorncable.com/assets_new/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script>
+    <!--c3 JavaScript -->
+    <script src="https://pack1.sakorncable.com/assets_new/libs/d3/dist/d3.min.js"></script>
+    <script src="https://pack1.sakorncable.com/assets_new/libs/c3/c3.min.js"></script>
+    <!-- Chart JS -->
+    <script src="https://pack1.sakorncable.com/dist/js/pages/dashboards/dashboard1.js"></script>
 
-
-
- 
-<!--
-        <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-        <p class="lead">
-          <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
-        </p>
--->
-
- 
-
-
-
-      <footer class="mastfoot mt-auto">
-        <div class="inner">
-          <p>Temperature Report TCT ComEdu,<a href="https://twitter.com/mdo"> Cover Template by @mdo</a>.</p>
-        </div>
-      </footer>
-    </div>
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
 
 
     <script type="text/javascript">
         
 
-      $(function(){
+      $(function(){ 
 
         
 
@@ -115,6 +196,10 @@
             $("#ImageCapture").attr("src",object[0].URLIMG);
             $("#QRCodeApp").attr("src","https://api.qrserver.com/v1/create-qr-code/?data="+APPURL+"&size=220x220&margin=0");
             $("#Temp").text(object[0].Temp);
+            $("#CountTotal").text(object[0].Total);
+            $("#TempAVG").text(object[0].TempMean);
+
+
 
 
           });
@@ -130,8 +215,8 @@
 
     </script>
 
+</body>
 
 
-
-  </body>
+<!-- Mirrored from www.wrappixel.com/demos/admin-templates/materialpro-bootstrap-latest/material-pro/src/minisidebar/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 15 Jul 2020 01:41:16 GMT -->
 </html>
