895708218c6112b24bd54ad01c207c9c496d5c64
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-09-04 13:13:14 +0700

    add

--- application/controllers/Devices.php
+++ application/controllers/Devices.php
@@ -51,7 +51,7 @@ class Devices extends CI_Controller
 			$this->mqtt->connect();
 			$this->mqtt->subscribe('php-mqtt/client/test', function ($topic, $message) {
 			    echo sprintf("Received message on topic [%s]: %s\n", $topic, $message);
-			}, 0);
+			}, 1);
 			$this->mqtt->loop(true);
 
 
