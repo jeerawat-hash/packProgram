e2ab6d19d4a6019a23ede75461e0bc2724f169e1
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-09-04 13:04:27 +0700

    add

--- application/views/page/mobile/test.php
+++ application/views/page/mobile/test.php
@@ -1,29 +1,42 @@
-
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.js" type="text/javascript"></script>
- 
-<script>
-client = new Paho.MQTT.Client("203.151.89.57", Number(1901), "clientId");
-client.onConnectionLost = onConnectionLost;
-client.onMessageArrived = onMessageArrived;
-
-client.connect({onSuccess:onConnect});
-
-function onConnect() {
-  console.log("onConnect");
-  client.subscribe("TEST/MQTT");
-  message = new Paho.MQTT.Message("Hello MQTT");
-  message.destinationName = "TEST/MQTT";
-  client.send(message);
-}
-
-function onConnectionLost(responseObject) {
-  if (responseObject.errorCode !== 0) {
-    console.log("onConnectionLost:"+responseObject.errorMessage);
-  }
-}
-
-function onMessageArrived(message) {
-  console.log("onMessageArrived:"+message.payloadString);
-}
-</script>
+<html>
+   <head>
+      <title>JavaScript MQTT WebSocket Example</title>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.js" type="text/javascript">
+     </script>
+     <script type = "text/javascript" language = "javascript">
+        var mqtt;
+        var reconnectTimeout = 2000;
+        var host="203.151.89.57"; //change this
+        var port=1901;
+        
+        function onConnect() {
+      // Once a connection has been made, make a subscription and send a message.
+    
+        console.log("Connected ");
+        //mqtt.subscribe("sensor1");
+        message = new Paho.MQTT.Message("Hello World");
+        message.destinationName = "sensor1";
+        mqtt.send(message);
+      }
+      function MQTTconnect() {
+        console.log("connecting to "+ host +" "+ port);
+        mqtt = new Paho.MQTT.Client(host,port,"clientjs");
+        //document.write("connecting to "+ host);
+        var options = {
+            timeout: 3,
+            onSuccess: onConnect,
+          
+         };
+         
+        mqtt.connect(options); //connect
+        }
+     
+      </script>
+   </head>
+     <body>
+   <h1>Main Body</h1>
+    <script>
+    MQTTconnect();
+    </script>
+   </body>  
+</html>
