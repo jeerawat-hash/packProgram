76722da73a19673fbc2cfb86a6a95a7ce0ce36f7
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-09-06 22:05:43 +0700

    add

--- assets/pages/mobile/js/Mobile_SmartWM.js
+++ assets/pages/mobile/js/Mobile_SmartWM.js
@@ -1,5 +1,18 @@
 $(function(){
- 	 
+ 	 	
+
+
+
+
+ 	 	
+ 	 		$("#SmartWM").on("click",function(){
+
+ 	 			$("#SmartWMModal").modal("show");
+ 	 			$("#PreloadSendData").hide();
+ 	 			
+ 	 		});
+ 
+
 			$("#SelectImageWM").on("click",function(){
 
 				    $('#WMQRCodeFile').trigger('click');
