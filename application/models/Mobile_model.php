981a3b9482ffb8234275d433621393219bdbb594
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-10-23 13:01:23 +0700

    add

--- application/models/Mobile_model.php
+++ application/models/Mobile_model.php
@@ -23,7 +23,7 @@ class Mobile_model extends CI_Model
      $this->mssql = $this->load->database("mssql",true);
 
 
-     return $this->mssql->query(" select CustomerID,TitleName+' '+CustomerName+' '+NoHome as CustomerINFO from Theparak3.dbo.Customer where CustomerID = '".$CustomerID."' and ProjectCode = '".$ProjectCode."' ")->result();
+     return $this->mssql->query(" select CustomerID,TitleName+' '+CustomerName+' '+NoHome as CustomerINFO from Theparak3.dbo.Customer where CustomerID = '".$CustomerID."'  ")->result();
 
 
   }
