4507e407631adb11b5d10a16553d46bad40d00f6
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-11-03 21:54:53 +0700

    add

--- application/views/page/mobile/mobile_home.php
+++ application/views/page/mobile/mobile_home.php
@@ -1234,14 +1234,14 @@
 
 
         
-        <script src="https://pack1.sakorncable.com/assets/pages/mobile/js/mobile_home.js?v=1000"></script>
-        <script src="https://pack1.sakorncable.com/assets/pages/mobile/js/Mobile_CCTV.js?v=1000"></script>
-        <script src="https://pack1.sakorncable.com/assets/pages/mobile/js/Mobile_CustomerCheckblanace.js?v=1000"></script>
-        <script src="https://pack1.sakorncable.com/assets/pages/mobile/js/Mobile_CustomerSendSlip.js?v=1000"></script>
-        <script src="https://pack1.sakorncable.com/assets/pages/mobile/js/Mobile_CustomerGetDataCarModal.js?v=1000"></script>
-        <script src="https://pack1.sakorncable.com/assets/pages/mobile/js/Mobile_SmartSwitch.js?v=1000"></script>
-        <script src="https://pack1.sakorncable.com/assets/pages/mobile/js/Mobile_SmartSecurity.js?v=1000"></script>
-        <script src="https://pack1.sakorncable.com/assets/pages/mobile/js/Mobile_SmartWM.js?v=1000"></script>
+        <script src="https://pack1.sakorncable.com/assets/pages/mobile/js/mobile_home.js?v=1001"></script>
+        <script src="https://pack1.sakorncable.com/assets/pages/mobile/js/Mobile_CCTV.js?v=1001"></script>
+        <script src="https://pack1.sakorncable.com/assets/pages/mobile/js/Mobile_CustomerCheckblanace.js?v=1001"></script>
+        <script src="https://pack1.sakorncable.com/assets/pages/mobile/js/Mobile_CustomerSendSlip.js?v=1001"></script>
+        <script src="https://pack1.sakorncable.com/assets/pages/mobile/js/Mobile_CustomerGetDataCarModal.js?v=1001"></script>
+        <script src="https://pack1.sakorncable.com/assets/pages/mobile/js/Mobile_SmartSwitch.js?v=1001"></script>
+        <script src="https://pack1.sakorncable.com/assets/pages/mobile/js/Mobile_SmartSecurity.js?v=1001"></script>
+        <script src="https://pack1.sakorncable.com/assets/pages/mobile/js/Mobile_SmartWM.js?v=1001"></script>
 
 
 
