59b3a3172f848348d9ab0d9edc0aba1370e1dd95
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-11-03 13:11:48 +0700

    add

--- application/controllers/Mobile_app.php
+++ application/controllers/Mobile_app.php
@@ -47,6 +47,15 @@ class Mobile_app extends CI_Controller
 	{
 
 
+			$header['page_name'] = 'ลูกบ้านอาคารชุด Pack1';
+			$header['page_focus'] = 'home';
+			
+			$FAQ["Project"] = "ยืนยันตัวตนด้วยหมายเลขห้องบ้านเอื้ออาทรPack1";
+			$FAQ["FAQ"] = "หมายเลขตึก-หมายเลขห้อง";
+
+			$this->load->view("page/template_mobile/header",$header);
+			$this->load->view("page/mobile/mobile_home",$FAQ);
+			$this->load->view("page/template_mobile/footer");
 
 
 
