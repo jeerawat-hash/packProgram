cdc163d8c7410973213be0ff4b93d8aaed4d9517
jeerawat <s6202041520016@email.kmutnb.ac.th>
2021-01-31 14:19:44 +0700

    add

---
+++ application/controllers/Sp.php
@@ -0,0 +1,43 @@
+<?php 
+/**
+ * 
+ */
+class Sp extends CI_Controller
+{
+	
+	function __construct()
+	{
+		
+		parent::__construct(); 
+ 		#$this->load->model("Monitor_model");
+
+
+	}
+
+	public function index($cust)
+	{
+ 		
+		if (!isset($cust)) {
+			
+			exit();
+		}
+
+
+		$this->load->view("page/recept/recept");
+
+ 
+
+		#ใบเสร็จค่าส่วนกลาง
+		#https://pack1.sakorncable.com/index.php/sp/101-001
+
+
+	}  
+
+
+
+
+
+}
+
+
+ ?>
