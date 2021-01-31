c8ba59a561fb0b8e409bb92cff5387c69b6c779c
jeerawat <s6202041520016@email.kmutnb.ac.th>
2021-01-31 17:26:19 +0700

    add

--- application/views/page/recept/recept.php
+++ application/views/page/recept/recept.php
@@ -25,32 +25,20 @@
       <h3>รายการ</h3>
         <table class="table">
           <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+            <tr> 
+              <th scope="col">หมายเลขใบเสร็จ</th>
+              <th scope="col">รายการ</th>
+              <th scope="col">ชำระ</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+
+            <tr> 
+              <td>0007000</td>
+              <td>ค่าส่วนกลาง</td>
+              <td> 500 บาท</td>
             </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
-            </tr>
+            
           </tbody>
         </table>
     </div>
