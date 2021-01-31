7f731960c48b5f5d58b8f96670d7453632ed1209
jeerawat <s6202041520016@email.kmutnb.ac.th>
2021-01-31 17:23:58 +0700

    add

--- application/views/page/recept/recept.php
+++ application/views/page/recept/recept.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>ใบเสร็จรับเงิน</title>
+  <title>ใบเสร็จรับเงิน นิติบุคคลสาครเคเบิล</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -11,26 +11,48 @@
 <body>
 
 <div class="jumbotron text-center">
-  <h1>My First Bootstrap Page</h1>
-  <p>Resize this responsive page to see the effect!</p> 
+  <h1>ใบเสร็จรับเงินดิจิตอล</h1>
+  <p>นิติบุคคล Pack1 เอื้ออาทรเทพารัก 3/1</p> 
 </div>
   
 <div class="container">
   <div class="row">
-    <div class="col-sm-4">
-      <h3>Column 1</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    <div class="col-sm-3">
+      <h3>ข้อมูล</h3>
+      <p>ชื่อ</p> 
     </div>
-    <div class="col-sm-4">
-      <h3>Column 2</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 3</h3>        
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    <div class="col-sm-9">
+      <h3>รายการ</h3>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
     </div>
   </div>
 </div>
---
+++ manual/script/sms/smsschedue.php
@@ -0,0 +1,35 @@
+ 
+<?php 
+
+ 
+
+  $a = mssql_connect('mssqlcon', 'sa', 'Sakorn123');
+
+
+   $Customer = mssql_query("  SELECT  distinct [CUST],b.*
+  FROM [Sakorn_Theparak3].[dbo].[CustomerPay_LOG] a 
+  join Sakorn_Theparak3.dbo.Customer b on a.CUST = b.CustomerID where b.ProjectCode = 'P5' and Telephone is not null  ");
+
+
+
+   		while ( $result = mssql_fetch_array($Customer) ) {
+   			
+
+   			mssql_query(" INSERT INTO [Sakorn_Theparak3].[dbo].[CustomerSms]
+           ([Telephone]
+           ,[CustomerID]
+           ,[StampDate] )
+     VALUES
+           ('".$result["Telephone"]."'
+           ,'".$result["CUST"]."'
+           ,'".date("Y-m-d")."') ");
+
+
+
+   		}
+
+ 
+  
+
+ ?>
+ 
